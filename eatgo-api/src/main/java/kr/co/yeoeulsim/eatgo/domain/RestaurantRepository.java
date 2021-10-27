6fcf518cc5d2d59fbe3ee26c9694240b1539b875
6161990 <6161990src@gmail.com>
2021-10-28 07:59:07 +0900

    의존성 주입 @SpyBeans

--- eatgo-api/src/main/java/kr/co/yeoeulsim/eatgo/domain/RestaurantRepository.java
+++ eatgo-api/src/main/java/kr/co/yeoeulsim/eatgo/domain/RestaurantRepository.java
@@ -1,33 +1,9 @@
 package kr.co.yeoeulsim.eatgo.domain;
 
-import org.springframework.stereotype.Repository;
-
-import java.util.ArrayList;
 import java.util.List;
 
+public interface RestaurantRepository {
+    List<Restaurant> findAll();
 
-public class RestaurantRepository {
-
-    private  List<Restaurant> restaurants = new ArrayList<>();
-
-    public RestaurantRepository(){
-        restaurants.add(new Restaurant(1004L, "Bob zip","seoul"));
-        restaurants.add(new Restaurant(2020L, "Cyber Food","seoul"));
-    }
-
-
-    public List<Restaurant> findAll() {
-        return restaurants;
-    }
-
-    public Restaurant findById(Long id) {
-        return restaurants.stream()
-                .filter(r -> r.getId().equals(id))
-                .findFirst()
-                .get();
-    }
-    // .orElse(null); get()대신 이렇게
-
-
-
+    Restaurant findById(Long id);
 }
---
+++ eatgo-api/src/main/java/kr/co/yeoeulsim/eatgo/domain/RestaurantRepositoryImpl.java
@@ -0,0 +1,37 @@
+package kr.co.yeoeulsim.eatgo.domain;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class RestaurantRepositoryImpl implements RestaurantRepository {
+
+    private List<Restaurant> restaurants = new ArrayList<>();
+
+    public RestaurantRepositoryImpl(){
+        restaurants.add(new Restaurant(1004L, "Bob zip","seoul"));
+        restaurants.add(new Restaurant(2020L, "Cyber Food","seoul"));
+    }
+
+
+    @Override
+    public List<Restaurant> findAll() {
+        return restaurants;
+    }
+
+    @Override
+    public Restaurant findById(Long id) {
+        return restaurants.stream()
+                .filter(r -> r.getId().equals(id))
+                .findFirst()
+                .get();
+    }
+    // .orElse(null); get()대신 이렇게
+
+
+
+}
