ebe1ada86ed098f6aac12e9e2827a4594632e210
6161990src <6161990src@gmail.com>
2021-12-27 10:05:39 +0900

    메뉴 관리 1

---
+++ eatgo-api/src/main/java/kr/co/yeoeulsim/eatgo/application/MenuItemService.java
@@ -0,0 +1,14 @@
+package kr.co.yeoeulsim.eatgo.application;
+
+import kr.co.yeoeulsim.eatgo.domain.MenuItem;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MenuItemService {
+
+    public void bulkUpdate(Long restaurantId, List<MenuItem> menuItems) {
+
+    }
+}
