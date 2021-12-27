3133d23db5a57b007e8f8a5d82622a25156cce44
6161990src <6161990src@gmail.com>
2021-12-28 08:38:51 +0900

    메뉴 아이템 수정과 삭제

--- eatgo-api/src/main/java/kr/co/yeoeulsim/eatgo/domain/MenuItem.java
+++ eatgo-api/src/main/java/kr/co/yeoeulsim/eatgo/domain/MenuItem.java
@@ -1,11 +1,9 @@
 package kr.co.yeoeulsim.eatgo.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -22,5 +20,10 @@ public class MenuItem {
     private Long restaurantId;
 
     private String name;
+
+    @Transient // DB에 넣지 않겠다.
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT) // boolean default = false : false가 아니면 포함해라
+    private boolean destroy;
+
 
 }
--- eatgo-api/src/main/java/kr/co/yeoeulsim/eatgo/domain/MenuItemRepository.java
+++ eatgo-api/src/main/java/kr/co/yeoeulsim/eatgo/domain/MenuItemRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 public interface MenuItemRepository extends CrudRepository<MenuItem, Long> {
 
     List<MenuItem> findAllByRestaurantId(Long restaurantId);
-
+    void deleteById(Long id); // id가 아니라 엔티티를 전부 넣어줘서 삭제할 때는 id에 지정한 타입으로 return 된다.
 }
