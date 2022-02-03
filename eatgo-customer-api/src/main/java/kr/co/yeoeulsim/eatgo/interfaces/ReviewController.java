8410d5f7dacb0ca9dd60462ebd493deaab3120cb
6161990src <6161990src@gmail.com>
2022-02-04 08:20:42 +0900

    * reviewService 시그니처 변경 적용 * Authentication 반영한 인증 코드 추가

--- eatgo-customer-api/src/main/java/kr/co/yeoeulsim/eatgo/interfaces/ReviewController.java
+++ eatgo-customer-api/src/main/java/kr/co/yeoeulsim/eatgo/interfaces/ReviewController.java
@@ -1,9 +1,11 @@
 package kr.co.yeoeulsim.eatgo.interfaces;
 
+import io.jsonwebtoken.Claims;
 import kr.co.yeoeulsim.eatgo.application.ReviewService;
 import kr.co.yeoeulsim.eatgo.domain.Review;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,10 +23,17 @@ public class  ReviewController {
 
     @PostMapping("/restaurants/{restaurantId}/reviews")
     public ResponseEntity<?> create(
+            Authentication authentication,
             @PathVariable("restaurantId") Long restaurantId,
             @Valid @RequestBody Review resource
     ) throws URISyntaxException {
-        Review review = reviewService.addReview(restaurantId, resource);
+        Claims claims = (Claims) authentication.getPrincipal();
+        String name = claims.get("userName", String.class);
+        Integer score = resource.getScore();
+        String description = resource.getDescription();
+
+        Review review = reviewService.addReview(restaurantId, name, score, description);
+
         String url = "/restaurants/" + restaurantId + "/reviews/" + review.getId();
         return ResponseEntity.created(new URI(url))
                 .body("{}");
