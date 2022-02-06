91963635cd481d9778e2dbd9eb7160200c927d91
6161990src <6161990src@gmail.com>
2022-02-07 07:59:38 +0900

    ReservationService 추가에 따른 Controller 수정 with Test

--- eatgo-customer-api/src/main/java/kr/co/yeoeulsim/eatgo/interfaces/ReservationController.java
+++ eatgo-customer-api/src/main/java/kr/co/yeoeulsim/eatgo/interfaces/ReservationController.java
@@ -1,5 +1,7 @@
 package kr.co.yeoeulsim.eatgo.interfaces;
 
+import kr.co.yeoeulsim.eatgo.application.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,10 +13,21 @@ import java.net.URISyntaxException;
 @RestController
 public class ReservationController {
 
+    @Autowired
+    private ReservationService reservationService;
+
     @PostMapping("/restaurants/{restaurantId}/reservations")
     public ResponseEntity<?> create(
             @PathVariable Long restaurantId
     ) throws URISyntaxException {
+
+        Long userId = 1L;
+        String name = "Tester";
+        String date = "2019-12-14";
+        String time = "20:00";
+        Integer partySize = 20;
+
+        reservationService.addReservation(restaurantId, userId, name, date, time, partySize);
         String url = "/restaurants/"+ restaurantId +"/reservations/1";
         return ResponseEntity.created(new URI(url)).body("{}");
     }
--- eatgo-customer-api/src/test/java/kr/co/yeoeulsim/eatgo/interfaces/ReservationControllerTest.java
+++ eatgo-customer-api/src/test/java/kr/co/yeoeulsim/eatgo/interfaces/ReservationControllerTest.java
@@ -1,12 +1,17 @@
 package kr.co.yeoeulsim.eatgo.interfaces;
 
+import kr.co.yeoeulsim.eatgo.application.ReservationService;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -17,10 +22,24 @@ class ReservationControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private ReservationService reservationService;
+
     @Test
     public void create() throws Exception {
-        mvc.perform(post("/restaurants/1004/reservations"))
+        Long userId = 1L;
+        String name = "Tester";
+        String date = "2019-12-14";
+        String time = "20:00";
+        Integer partySize = 20;
+
+        mvc.perform(post("/restaurants/1004/reservations")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"userId\":1,\"name\":\"Tester\",\"date\":\"2019-12-14\",\"time\":\"20:00\",\"partySize\":20}"))
                 .andExpect(status().isCreated());
+
+        verify(reservationService).addReservation(
+                eq(1004L), eq(userId), eq(name), eq(date), eq(time), eq(partySize));
     }
 
 }
