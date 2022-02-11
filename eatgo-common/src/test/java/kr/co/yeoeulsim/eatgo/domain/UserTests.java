973f8ba9c5e9c098acf78e05e88c8fc7c93fa95b
6161990src <6161990src@gmail.com>
2022-02-11 14:29:01 +0900

    Parameterized Test

--- eatgo-common/src/test/java/kr/co/yeoeulsim/eatgo/domain/UserTests.java
+++ eatgo-common/src/test/java/kr/co/yeoeulsim/eatgo/domain/UserTests.java
@@ -1,8 +1,14 @@
 package kr.co.yeoeulsim.eatgo.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class UserTests {
 
@@ -35,6 +41,52 @@ public class UserTests {
 
         assertThat(user.isRestaurantOwner()).isTrue();
         assertThat(user.getRestaurantId()).isEqualTo(1004L);
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @EmptySource // @NullAndEmptySource
+    @ValueSource(strings = {" ","  ", "\t", "\n"})
+    public void repeatTestUserInvalidEmail(String text) {
+        User user = User.builder()
+                .email(text)
+                .build();
+        String email = user.getEmail();
+        assertTrue(email == text);
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @EmptySource
+    @ValueSource(strings = {" ","  ", "\t", "\n"})
+    public void repeatTestUserInvalidEmail2(String text, TestReporter testReporter) {
+        User user = User.builder()
+                .email(text)
+                .build();
+        String email = user.getEmail();
+        assertTrue(email+ "TEXT"  == text);
+    }
+
+    @DisplayName("반복되는 테스트마다 인수를 알려줍니다.")
+    @ParameterizedTest(name = "{index} ==> Email can't contains '{arguments}'")
+    @ValueSource(strings = {"@","!", "*", "#"})
+    public void repeatTestUserInvalidEmail3(String text) {
+        User user = User.builder()
+                .email(text)
+                .build();
+        String email = user.getEmail();
+        assertTrue(email.concat("@") == text);
+    }
+
+    @DisplayName("")
+    @ParameterizedTest(name = "{index} ==> Email can't contains '{arguments}'")
+    @ValueSource(strings = {"@","!", "*", "#"})
+    public void repeatTestUserInvalidEmail4(String text) {
+        User user = User.builder()
+                .email(text)
+                .build();
+        String email = user.getEmail();
+        assertTrue(email.concat("@") == text);
     }
 
 }
